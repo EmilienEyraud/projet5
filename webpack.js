424f575a81b179f98c9c30f7e977268ad2ae5ed3
Emilieneyraud <direction@groupe-socom.fr>
2020-10-24 18:42:46 +0200

    page_produit

--- webpack.js
+++ webpack.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   mode: "production",
   entry: {
-    app: "./src/script.js"
+    app: "script.js"
   },
   output: {
-    filename: "init.bundle.js",
+    filename: "app.bundle.js",
     path: path.resolve(__dirname, "dist")
   }
 };
